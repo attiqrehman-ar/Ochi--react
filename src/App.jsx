f64846198c86b878f6eae7cbc292b34efbc1cb5b
attiqrehman-ar <attiqrehman130@gmail.com>
2025-03-13 02:28:50 +0500

    create reacte app and setup with tailwend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './components/Navbar'
+import Navbar from './components/Navbar'  
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+
+<Navbar /> 
+    </div>
+     
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='text-white '>
+      hi
+    </div>
+  )
+}
+
+export default Navbar
