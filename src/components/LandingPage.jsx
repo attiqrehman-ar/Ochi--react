1cfa0ab4d0ee29daf3dc393f6d4b91a24bdec21d
attiqrehman-ar <attiqrehman130@gmail.com>
2025-03-13 03:11:25 +0500

    add text to the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>
-      
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className='textstructure font-["FounderGrotesk"] mt-40'>
+            <div className='masker'>
+            <h1 className='uppercase  text-8xl leading-[4vw] tracking-tighter font-regular '>We Create</h1>
+            </div>
+            <div className='masker'>
+            <h1 className='uppercase  text-8xl leading-[4vw] tracking-tighter font-regular '>Eye Opening</h1>
+            </div>
+            <div className='masker'>
+            <h1 className='uppercase  text-8xl leading- tracking-tighter font-regular '>Projects</h1>
+            </div>
+        </div>
     </div>
   )
 }
