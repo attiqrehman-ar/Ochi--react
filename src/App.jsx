ce7962abddca93c5d733a18928d1e1e97d8ca4fe
attiqrehman-ar <attiqrehman130@gmail.com>
2025-03-13 02:57:08 +0500

    create landing page file and connect to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'  
+import LandingPage from './components/LandingPage'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
