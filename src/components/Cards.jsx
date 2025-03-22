7cfd775d97d4e2b9704a536b3c21b9ef8d200cc1
ecomback-official <sales@ecomback.com>
2025-03-22 19:25:23 +0500

    add images to other cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,12 +6,16 @@ function Cards() {
        <div className=' cardcontainer w-1/2 h-[50vh]'>
        <div className='relative card rounded-lg w-full h-full  bg-[#004D43] flex items-center justify-center'>
         <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
-       <button className='absolute left-10 bottom-10'>Hi</button>
+       <button className='absolute left-10 bottom-10 px-5 py-1 border-1 rounded-full'> &copy; 2024-present</button>
        </div>
        </div>
        <div className=' cardcontainer w-1/2 flex gap-5 h-[50vh]' >
-       <div className='w-full rounded-lg h-full  bg-[#192826]'></div> 
-       <div className='w-full rounded-lg h-full  bg-[#192826]'></div> 
+       <div className=' card relative flex items-center justify-center w-full rounded-lg h-full  bg-[#192826]'>
+       <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+       <button className='absolute left-10 bottom-10 px-5 py-1 border-1 rounded-full'> &copy; 2024-present</button></div> 
+       <div className=' card relative  flex items-center justify-center w-full rounded-lg h-full  bg-[#192826]'>
+       <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+       <button className='absolute left-10 bottom-10 px-5 py-1 border-1 rounded-full'> &copy; 2024-present</button></div> 
        </div>
     </div>
   )
