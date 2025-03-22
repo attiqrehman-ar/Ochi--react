654939a3d66c6e5810966c416004f6150da0a92f
ecomback-official <sales@ecomback.com>
2025-03-22 17:32:30 +0500

    add featured section after eye

---
+++ src/components/Featured.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className='w-full py-10'>
+      <div className='w-full px-10'>
+        <h1>Featured</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
