ecea9b4531393f609961e3c9cdbb44660e4f658a
ecomback-official <sales@ecomback.com>
2025-03-22 17:28:00 +0500

    add animation on mouse perfectly

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -10,7 +10,7 @@ function Eyes() {
       let deltaX = mouseX - window.innerWidth / 2;
       let deltaY = mouseY - window.innerHeight / 2;
       var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-      setrotate(angle);
+      setrotate(angle-180);
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -22,16 +22,16 @@ function Eyes() {
       <div className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
         <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
           <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-            <div className='relative w-2/3 h-2/3 bg-zinc-900 rounded-full'>
-              <div style={{transform: `translate(-50%, -50%) rotate(${angle}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
-                <div className='w-10 h-10 bg-zinc-100 rounded-full'></div>
+            <div className='relative w-2/3 h-2/3 bg-zinc-900 rounded-full overflow-hidden'>
+              <div style={{transform: `translate(-50%, -50%) rotate(${angle}deg)`}} className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 h-10'>
+                <div className='w-8 h-8 bg-zinc-100 rounded-full'></div>
               </div>
             </div>
           </div>
           <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-            <div className='relative w-2/3 h-2/3 bg-zinc-900 rounded-full'>
-              <div style={{transform: `translate(-50%, -50%) rotate(${angle}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
-                <div className='w-10 h-10 bg-zinc-100 rounded-full'></div>
+            <div className='relative w-2/3 h-2/3 bg-zinc-900 rounded-full overflow-hidden'>
+              <div style={{transform: `translate(-50%, -50%) rotate(${angle}deg)`}} className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 h-10'>
+                <div className='w-8 h-8 bg-zinc-100 rounded-full'></div>
               </div>
             </div>
           </div>
