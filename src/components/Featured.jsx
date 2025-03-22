895b4a24a1fad794abc8a3c5a9e97353d59b7194
ecomback-official <sales@ecomback.com>
2025-03-22 17:43:37 +0500

    add cards and styles

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 function Featured() {
   return (
-    <div className='w-full py-10'>
-      <div className='w-full px-10'>
-        <h1>Featured</h1>
+    <div className='w-full py-20'>
+      <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-20 mb-10'>
+        <h1 className='text-8xl font-[NueueMontreal] tracking-tight'>Featured Projects</h1>
+
       </div>
+      <div className='px-20'>
+      <div className="cards w-full flex gap-10">
+            <div className='card w-1/2 h-[90vh] bg-red-500'  ></div>
+            <div className='card w-1/2 h-[90vh] bg-red-500'  ></div>
+        </div>
+      </div>
+
     </div>
   )
 }
