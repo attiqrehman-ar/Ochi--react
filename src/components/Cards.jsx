8afc2708b7bf22171c02ff0be0f0c386e820dcb7
ecomback-official <sales@ecomback.com>
2025-03-22 19:20:16 +0500

    add button and images on the 1st card section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,8 +4,9 @@ function Cards() {
   return (
     <div className='w-full h-screen flex gap-5 pb-10 items-center px-20'>
        <div className=' cardcontainer w-1/2 h-[50vh]'>
-       <div className='card rounded-lg w-full h-full  bg-[#004D43]'>
+       <div className='relative card rounded-lg w-full h-full  bg-[#004D43] flex items-center justify-center'>
         <img className="w-32" src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+       <button className='absolute left-10 bottom-10'>Hi</button>
        </div>
        </div>
        <div className=' cardcontainer w-1/2 flex gap-5 h-[50vh]' >
