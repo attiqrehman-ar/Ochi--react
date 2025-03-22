958e3fd8de59f98cf7ae5e2aaccdabee368faae1
ecomback-official <sales@ecomback.com>
2025-03-22 16:44:14 +0500

    add perfect eye structure for scroll animation

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -6,10 +6,16 @@ function Eyes() {
       <div  className=' relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")]'>
       <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   '>
       <div className=' flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-        <div className=' w-2/3 h-2/3 bg-zinc-900 rounded-full'></div>
+        <div className=' flex items-center justify-center  w-2/3 h-2/3 bg-zinc-900 rounded-full'>
+          <div className=' w-1/4 h-1/4 bg-zinc-100 rounded-full'>
+          </div>
+        </div>
       </div>
       <div className=' flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full'>
-      <div className=' w-2/3 h-2/3 bg-zinc-900 rounded-full'></div>
+      <div className=' flex items-center justify-center w-2/3 h-2/3 bg-zinc-900 rounded-full'>
+      <div className=' w-1/4 h-1/4 bg-zinc-100 rounded-full'>
+          </div>
+      </div>
 
       </div>
       </div>
