90df6b68575f020a878fde986351d34be528f9a4
ecomback-official <sales@ecomback.com>
2025-03-22 21:20:48 +0500

    add animation on marquee componenet and smooth transit

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,9 +5,9 @@ function Marquee() {
   return (
     <div className='w-full  bg-[#004D43] py-20 rounded-tr-3xl rounded-tl-3xl'>
         <div className='text border-t-2 border-b-2 border-zinc300 flex gap-10 whitespace-nowrap overflow-hidden'>
-            <h1 className='text-[15vw] font-["FounderGrotesk"] leading-none font-semibold'> We are Web Developer</h1>
-            <h1 className='text-[15vw] font-["FounderGrotesk"] leading-none font-semibold'>We Build your E-commerce Store</h1>
-            <h1 className='text-[15vw] font-["FounderGrotesk"] leading-none  font-semibold'>We Rank your site and make you unique</h1>
+            <motion.h1  initial={{x:"0"}} animate={{x:"-100%"}} transition={{ repeat: Infinity, ease:"linear", duration:5}} className='text-[15vw] font-["FounderGrotesk"] leading-none font-semibold'> We Build your E-commerce Store</motion.h1>
+            <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{ repeat: Infinity, ease:"linear", duration:5}} className='text-[15vw] font-["FounderGrotesk"] leading-none font-semibold'>We Build your E-commerce Store</motion.h1>
+            {/* <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{ repeat: Infinity, ease:"linear", duration:5}} className='text-[15vw] font-["FounderGrotesk"] leading-none  font-semibold'>We Rank your site and make you unique</motion.h1> */}
 
         </div>
     </div>
